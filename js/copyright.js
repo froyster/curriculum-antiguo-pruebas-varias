d915792cc7ce34021ec589bd23a15f601998f984
froyster <froyster@gmail.com>
2015-11-16 20:35:02 +0100

    Revert "restificado copright y css varias cosas"
    This reverts commit 20f28141af7ed81ccc0b9dbc430733cdc5f8e5d3.

--- js/copyright.js
+++ js/copyright.js
@@ -5,30 +5,18 @@ window.onload=function(){
 
 function fecha(){
     var hoy = new Date();
-    var cumple = new Date("05/16/1978 09:30:15");
-    var cp = hoy.getFullYear(); //Dato del Coptright
+    var cumple = new Date("05/19/1978 09:30:15");
+    var cp = hoy.getFullYear();
     var web = document.getElementById("cp");
     var difAno = hoy.getFullYear() - cumple.getFullYear();
     var difMeses = hoy.getMonth() - cumple.getMonth();
-        if (hoy.getDate()<cumple.getDate()) {
-            difMeses -=1;
-        };
-       
+    //calculo del numero de dias esto es mas complicado utilizamos getTime(); devuelde el dato en milisegundos asi
+    //que a transformarlo en dias;
     var dia = 1000*60*60*24;//transformamos los milisegundos en un segundo estos en un minuto y estos en un dia.
     
-    var cumpleDia = cumple.getDate();
-    var hoyDia = hoy.getDate();
-    var dias = (function(){
-        var pequeno = Math.min(cumpleDia,hoyDia);
-        var grande = Math.max(cumpleDia,hoyDia);
-        if (pequeno === hoyDia) {
-            return  ((hoy.getTime() - cumple.getTime())/dia) - ((difAno*365)+(difMeses*30)); //error no se que hacer
-        } else{
-            return hoyDia - cumpleDia ;
-        };
-        
-    });
-    var difDias = (Math.round( dias()/* <--ejecutamos la funcion*/)); //error no consigo calcular el dia.
-
-    web.innerHTML = " " + cp + "<< >> Luchando " + difAno + " Años " + difMeses + " Meses " + difDias + " Dias ";  
+    var dif = hoy.getTime() - cumple.getTime();
+        //error
+    
+    var difDias = (Math.round(Math.abs( dif/dia )))  ; //error no consigo calcular el dia.
+    web.innerHTML = " " + cp + " << >> Luchando " + difAno + " Años " + difMeses + " Meses " + difDias + " Dias "   ; 
 }   
