fdd49d619744e116d83360e9bff216aa0e283e2e
froyster <froyster@gmail.com>
2015-11-17 20:58:34 +0100

    Merge arreglos

--- webs/menuJquerrryUI/script.js
+++ webs/menuJquerrryUI/script.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-    $("#menu").accordion();
-=======
-$(document).ready(function(){
-    $("#menu").accordion();
->>>>>>> 20f28141af7ed81ccc0b9dbc430733cdc5f8e5d3
-    });
+$(document).ready(function() {
+  $("#menu").accordion();
+
+});
