42756bfe1c6b2fff8bbf4c7e17f2b7a4bc4cb59e
froyster <froyster@gmail.com>
2015-11-18 21:18:12 +0100

    Arreglo precios y errores en web del Lavado

--- webs/curriculo/res/access.inc.php
+++ webs/curriculo/res/access.inc.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 // Users data
@@ -24,31 +23,5 @@ $imSettings['access']['admins'] = array('l2yfg4e3');
 $imSettings['access']['pages'] = array(
 );
 
-=======
-<?php
 
-// Users data
-$imSettings['access']['users'] = array(
-	'admin' => array(
-		'id' => 'l2yfg4e3',
-		'name' => 'Admin',
-		'password' => '9v9g9w9f',
-		'page' => 'index.html'
-	),
-	'nuevousuario' => array(
-		'id' => '8kb7ud9s',
-		'name' => 'Nuevousuario',
-		'password' => 'gq54qea1',
-		'page' => 'index.html'
-	)
-);
-
-// Admins list
-$imSettings['access']['admins'] = array('l2yfg4e3');
-
-// Page/Users permissions
-$imSettings['access']['pages'] = array(
-);
-
->>>>>>> 20f28141af7ed81ccc0b9dbc430733cdc5f8e5d3
 // End of file access.inc.php
--- webs/curriculo/res/imlogout.php
+++ webs/curriculo/res/imlogout.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 @require_once("x5engine.php");
 
@@ -6,13 +6,5 @@ $pa = new imPrivateArea();
 $pa->logout();
 header("Location: ../");
 
-=======
-<?php
-@require_once("x5engine.php");
 
-$pa = new imPrivateArea();
-$pa->logout();
-header("Location: ../");
-
->>>>>>> 20f28141af7ed81ccc0b9dbc430733cdc5f8e5d3
 // End of file imlogout.php
